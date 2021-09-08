ac4e26a82e5b0153aea620dfe9df59cc017a197d
Rostyslav Motorin <rostyslav.motorin@nixsolutions.com>
2021-09-08 12:13:14 +0300

    Add interface IRepair

---
+++ SeaBattleDM/Interfaces/IRepair.cs
@@ -0,0 +1,15 @@
+﻿using SeaBattleDM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattleDM.Interfaces
+{
+    interface IRepair
+    {
+        public int RangeRepair { get; }
+        public void Repairs(Point point);
+    }
+}
