17b3c5668839482d22482884fdb1fd049f022402
Rostyslav Motorin <rostyslav.motorin@nixsolutions.com>
2021-09-08 11:53:18 +0300

    Add struct Point

---
+++ SeaBattleDM/Models/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattleDM.Models
+{
+    struct Point
+    {
+        public readonly int X { get; }
+        public readonly int Y { get; }
+        public bool IsBlock { get; set; }
+
+        public Point(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+            IsBlock = false;
+        }
+    }
+}
