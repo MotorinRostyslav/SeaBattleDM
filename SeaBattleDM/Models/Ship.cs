1f95c3dd9fad781b369c5dc56f764951d28b2cc7
Rostyslav Motorin <rostyslav.motorin@nixsolutions.com>
2021-09-08 13:21:28 +0300

    Add abstract class Ship

---
+++ SeaBattleDM/Models/Ship.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattleDM.Models
+{
+    abstract class Ship
+    {
+        abstract public int Length { get; }
+        abstract public int Speed { get; }
+        abstract public string Type { get; }
+        abstract public string Direction { get; set; }
+        public virtual void Move(string direction) { }
+        abstract public  void GetState();
+        public static bool operator ==(Ship ship1, Ship ship2)
+        {
+            if(ship1.Length == ship2.Length && ship1.Speed == ship2.Speed && ship1.Type == ship2.Type)
+                return true;
+            else return false;
+        }
+        public static bool operator !=(Ship ship1, Ship ship2)
+        {
+            if (ship1.Length == ship2.Length && ship1.Speed == ship2.Speed && ship1.Type == ship2.Type)
+                return false;
+            else return true;
+        }
+    }
+}
