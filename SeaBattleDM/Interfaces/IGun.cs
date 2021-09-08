4e88d4736f27037bf59c549d783f9e28d40f7791
Rostyslav Motorin <rostyslav.motorin@nixsolutions.com>
2021-09-08 11:59:31 +0300

    Add interface IGun

---
+++ SeaBattleDM/Interfaces/IGun.cs
@@ -0,0 +1,15 @@
+﻿using SeaBattleDM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattleDM.Interfaces
+{
+    interface IGun
+    {
+        public int rangeFire { get; set; }
+        public void Fire(Point point);
+    }
+}
