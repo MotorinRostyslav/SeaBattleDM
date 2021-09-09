7874a5d5a2b4d90cf52280398091051f69490951
Rostyslav Motorin <rostyslav.motorin@nixsolutions.com>
2021-09-09 17:13:21 +0300

    Add class PositionService

---
+++ SeaBattleDM/Services/PositionService.cs
@@ -0,0 +1,27 @@
+﻿using SeaBattleDM.Models;
+
+
+namespace SeaBattleDM.Services
+{
+    class PositionService
+    {
+
+        public static void LockPoint(Point[] position)
+        {
+            for (int i = 0; i < position.Length; i++)
+            {
+                position[i].IsBlock = true;
+            }
+        }
+
+        public static bool CheckPosition(Point[] position)
+        {
+            bool checker = false;
+            foreach (var point in position)
+            {
+                if (point.IsBlock == true) checker = true;
+            }
+            return checker;
+        }
+    }
+}
